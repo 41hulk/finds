60e56c9f2e9456536ca3a7e8133b767beb69ab05
Ntare Guy <gntare2@gmail.com>
2024-11-09 23:53:39 +0200

    implement auth provider

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,34 +2,34 @@ class UserModel {
   final String uid;
   final String email;
   final String name;
-  final String photoUrl;
-  final String createdAt;
+  final String? photoUrl;
+  final DateTime createdAt;
 
   UserModel({
     required this.uid,
     required this.email,
     required this.name,
-    required this.photoUrl,
+    this.photoUrl,
     required this.createdAt,
   });
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      uid: json['uid'],
-      email: json['email'],
-      name: json['name'],
-      photoUrl: json['photoUrl'],
-      createdAt: json['createdAt'],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toMap() {
     return {
       'uid': uid,
       'email': email,
       'name': name,
       'photoUrl': photoUrl,
-      'createdAt': createdAt,
+      'createAt': createdAt.toIso8601String(),
     };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'],
+      email: map['email'],
+      name: map['name'],
+      photoUrl: map['photoUrl'],
+      createdAt: DateTime.parse(map['createAt']),
+    );
   }
 }
