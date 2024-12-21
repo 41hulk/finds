3486a5d41a7524a3625a10543c79a635c91e40ff
Ntare Guy <gntare2@gmail.com>
2024-12-21 06:22:39 +0200

    ref: profile page polish

--- lib/screens/explorer/explorer_screen.dart
+++ lib/screens/explorer/explorer_screen.dart
@@ -3,7 +3,6 @@ import 'package:finds/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../provider/property_provider.dart';
-import 'property_list.dart';
 
 class ExplorerScreen extends StatefulWidget {
   const ExplorerScreen({super.key});
